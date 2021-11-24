f1778001e66ce0d00fe9dc6c3c0a23f88f2f262a
Huiling Loh <huiling_loh_97@hotmail.com>
2021-11-24 15:18:42 +0800

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database"
+Flat.destroy_all
+
+puts "Creating flats..."
+
+5.times do |i|
+  Flat.create!({
+    name: Faker::JapaneseMedia::OnePiece.location,
+    address: Faker::Address.street_address,
+    description: Faker::Quotes::Shakespeare.romeo_and_juliet_quote,
+    price_per_night: rand(75..150),
+    number_of_guests: rand(1..5)
+  })
+end
+
+puts "Flats created!"
